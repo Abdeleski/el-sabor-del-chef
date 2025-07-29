cd5c7de2055cf455e743eff1f114263cece6d095
Abdeleski <aayyad@iescarlesrahola.net>
2025-07-29 18:23:17 +0200

    Actualizar SaborBot

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,7 @@ script.src = "https://dashboard.retellai.com/retell-widget.js";
 script.dataset.publicKey = "public_key_53f3288025b7e78e3161e"; 
 script.dataset.agentId = "agent_29a7741711c0b3593df62ab527";
 script.dataset.agentVersion = "0";
-script.dataset.title = "Chat de Retell";
+script.dataset.title = "SaborBot";
 document.body.appendChild(script);
 // ======================================
 
